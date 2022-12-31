494028b714920bcea59a968d346cb702b7c49cca
romfantast <romfantast@gmail.com>
2022-12-31 15:09:37 +0200

    fix

--- src/components/DeleteButton/DeleteButton.jsx
+++ src/components/DeleteButton/DeleteButton.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import css from './Button.module.css';
+import css from './DeleteButton.module.css';
 
 export default class Button extends Component {
   render() {
